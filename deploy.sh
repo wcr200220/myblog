2b75e9d272a15fdaa3d33516856170b916213dbb
wang.jianyong <wang.jianyong@iwhalecloud.com>
2020-06-19 10:23:34 +0800

    推送到github静态文件脚本修改

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,14 @@ yarn run build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-echo 'www.wcr200220.cc' > CNAME
+#echo 'www.xxx.xx' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://wcr200220.github.io
+# use https：git@github.com:wcr200220/wcr200220.github.io.git
 git push -f git@github.com:wcr200220/wcr200220.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
